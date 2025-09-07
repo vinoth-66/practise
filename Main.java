5985eef2ecea30f8848738e1ab276e95019c8119
vinoth-66 <vinothtechstack@gmail.com>
2025-09-07 19:46:34 +0530

    modified Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 public class Main{
-public static void main(String[] args) {
-    System.out.println("hi welcome to java!....");
-}
+    public static void main(String[] args) {
+        System.out.println("hi welcome to java!....");
+    }
 }
